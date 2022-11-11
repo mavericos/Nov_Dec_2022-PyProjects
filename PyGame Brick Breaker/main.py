8e1fdfdde2ddd372acaf6a302395bd1a2c811b23
Dzver <41322733+mavericos@users.noreply.github.com>
2022-11-11 19:22:58 +0200

    not finished yet

---
+++ PyGame Brick Breaker/main.py
@@ -0,0 +1,109 @@
+import pygame
+import math
+
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Brick Breaker")
+
+FPS = 60
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 15
+BALL_RADIUS = 10
+
+class Paddle:
+    VEL = 5
+
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+
+    def move(self, direction=1):
+        self.x = self.x + self.VEL * direction
+class Ball:
+    VEL = 5
+
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.x_vel = 2
+        self.y_vel = -self.VEL
+
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def set_vel(self, x_vel, y_vel):
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+def draw(win, paddle, ball):
+    win.fill("white")
+    paddle.draw(win)
+    ball.draw(win)
+    pygame.display.update()
+
+def ball_collision(ball):
+    if ball.x - BALL_RADIUS <= 0 or ball.x + BALL_RADIUS >= WIDTH:
+        ball.set_vel(ball.x_vel * -1, ball.y_vel)
+    if ball.y + BALL_RADIUS >= HEIGHT or ball.y - BALL_RADIUS <= 0:
+        ball.set_vel(ball.x_vel, ball.y_vel * -1)
+
+def ball_paddle_collision(ball, paddle):
+    if not (ball.x <= paddle.x * paddle.width and ball.x >= paddle.x):
+        return
+    if not (ball.y + ball.radius >= paddle.y):
+        return
+
+    paddle_center = paddle.x + paddle.width/2
+    distance_to_center = ball.x - paddle_center
+
+    percent_width = distance_to_center / paddle.width
+    angle = percent_width * 90
+    angle_radians = math.radians(angle)
+
+
+def main():
+    clock = pygame.time.Clock()
+
+    paddle_x = WIDTH/2 - PADDLE_WIDTH/2
+    paddle_y = HEIGHT - PADDLE_HEIGHT - 5
+    paddle = Paddle(paddle_x, paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT, 'brown')
+    ball = Ball(WIDTH/2, paddle_y - BALL_RADIUS, BALL_RADIUS, "brown")
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and paddle.x - paddle.VEL >=0:
+            paddle.move(-2)
+        if keys[pygame.K_RIGHT] and paddle.x + paddle.width + paddle.VEL <= WIDTH:
+            paddle.move(2)
+
+
+        ball.move()
+        ball_collision(ball)
+        draw(win, paddle, ball)
+
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main()
